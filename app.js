baa6761aa425445bd8d5832a5ac03b41a01bfcf3
rampemus <pasi.toivanen@live.com>
2019-08-25 11:34:11 +0300

    Built front-end accessible from http server

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const mockData = require('./mockData.json')
 
 app.use(cors())
 
+app.use(express.static('build'))
+
 //mock-server
 app.get('/wfs', ( request, response) => {
     response.send(mockData)
 })
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello There!</h1>')
-})
+
 
 module.exports = app
---
+++ build/precache-manifest.2a032075f428963ad626846dd17b276a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39df0d41507a76adee2b433f21489650",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8f7a020bb6751f98aa1",
+    "url": "/static/css/main.9ffa24cc.chunk.css"
+  },
+  {
+    "revision": "83685ab76dd1e92d4977",
+    "url": "/static/js/2.297207de.chunk.js"
+  },
+  {
+    "revision": "a8f7a020bb6751f98aa1",
+    "url": "/static/js/main.c8f5b1c8.chunk.js"
+  },
+  {
+    "revision": "e86913e2cc12e1369564",
+    "url": "/static/js/runtime~main.045affa5.js"
+  }
+]);
